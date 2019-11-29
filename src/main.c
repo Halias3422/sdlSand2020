7f615b09701d4dc2c44a9c0ac9f10077616bdde9
Halias3422 <v.desaintexupery@hotmail.fr>
2019-11-29 03:44:32 +0100

    made display more generic + faster sand fall but buggy

--- src/main.c
+++ src/main.c
@@ -21,12 +21,13 @@ void			clean_before_exiting(t_sdl *sdl, t_pixel ***pixel)
 	}
 }
 
-void			init_pixel_tab(t_pixel ***pixel)
+void			init_pixel_tab(t_pixel ***pixel, t_sdl *sdl)
 {
-	for (int y = 0; y < 1080; y++)
+	printf("window+h = %d\n", sdl->window_h);
+	for (int y = 0; y < sdl->window_h; y++)
 	{
-		pixel[y] = (t_pixel**)malloc(sizeof(t_pixel*) * 1920);
-		for (int x = 0; x < 1920; x++)
+		pixel[y] = (t_pixel**)malloc(sizeof(t_pixel*) * sdl->window_w);
+		for (int x = 0; x < sdl->window_w; x++)
 		{
 			pixel[y][x] = (t_pixel*)malloc(sizeof(t_pixel));
 			pixel[y][x]->filled = 0;
@@ -40,24 +41,40 @@ void			update_sand_on_screen(t_sdl *sdl, t_pixel ***pixel)
 
 	srand(time(NULL));
 	SDL_render_clear(sdl, sdl->renderer);
-	printf("je rentre\n");
-	for (int y = 0; y < 1000; y++)
+	for (int y = 0; y < sdl->window_h; y++)
 	{
-		for (int x = 0; x < 1920; x++)
+		for (int x = 0; x < sdl->window_w; x++)
 		{
-			if (y < 500 && pixel[y][x]->filled == 1 && pixel[y + 2][x]->filled == 0)
+			if (y < sdl->window_h - 100 && pixel[y][x]->filled == 1 && pixel[y + 1][x]->filled == 0)
 			{
 				pixel[y][x]->filled = 0;
-				if (pixel[y + 3][x]->filled == 0 && pixel[y + 4][x]->filled == 0)
-					pixel[y + 4][x] ->filled = 2;
-				else if (pixel[y + 3][x]->filled == 0)
-					pixel[y + 3][x]->filled = 2;
-				else
-					pixel[y + 2][x]->filled = 2;
+				for (int i = 8; i > 0; i--)
+				{
+					int obstructed = 0;
+					if (pixel[y + i][x]->filled == 0)
+					{
+						for (int j = i - 1; j > 0; j--)
+						{
+							if (pixel[y + j][x] != 0)
+								obstructed = 1;
+						}
+						if (obstructed == 1)
+						{
+							pixel[y + i][x]->filled = 2;
+							break ;
+						}
+					}
+				}
+				//if (pixel[y + 3][x]->filled == 0 && pixel[y + 4][x]->filled == 0)
+					//pixel[y + 4][x] ->filled = 2;
+				//else if (pixel[y + 3][x]->filled == 0)
+					//pixel[y + 3][x]->filled = 2;
+				//else
+					//pixel[y + 2][x]->filled = 2;
 				dst.x = x;
 				dst.y = y;
 			}
-			else if (x > 0 && x < 1079 && y < 500 && pixel[y][x]->filled == 1 && pixel[y + 2][x]->filled == 1 && (pixel[y + 2][x - 2]->filled == 0 || pixel[y + 2][x + 2]->filled == 0))
+			else if (x > 1 && x < sdl->window_w && y < sdl->window_h  - 100 && pixel[y][x]->filled == 1 && pixel[y + 2][x]->filled == 1 && (pixel[y + 2][x - 2]->filled == 0 || pixel[y + 2][x + 2]->filled == 0))
 			{
 				if (pixel[y + 2][x - 2]->filled == 0 && pixel[y + 2][x + 2] == 0)
 				{
@@ -81,10 +98,9 @@ void			update_sand_on_screen(t_sdl *sdl, t_pixel ***pixel)
 			}
 		}
 	}
-	printf("je sors\n");
-	for (int y = 0; y < 1080; y++)
+	for (int y = 0; y < sdl->window_h; y++)
 	{
-		for (int x = 0; x < 1920; x++)
+		for (int x = 0; x < sdl->window_w; x++)
 		{
 			if (y < 1080 && pixel[y][x]->filled == 2)
 			{
@@ -116,23 +132,23 @@ int				main(void)
 		failure_exit_program("Getting display mode", sdl);
 	sdl->window_h = display.h;
 	sdl->window_w = display.w;
-	pixel = (t_pixel***)malloc(sizeof(t_pixel**) * 1080);
-	init_pixel_tab(pixel);
+	pixel = (t_pixel***)malloc(sizeof(t_pixel**) * display.h);
+	init_pixel_tab(pixel, sdl);
 	SDL_apply_color_to_renderer(black, sdl);
 	SDL_render_clear(sdl, sdl->renderer);
 	SDL_RenderPresent(sdl->renderer);
 	while (quit != 1)
 	{
 		state = SDL_GetKeyboardState(NULL);
-		if (SDL_PollEvent(&event) && (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEMOTION))
+		if (SDL_PollEvent(&event) && (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEMOTION || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE)))
 		{
-			if (SDL_GetMouseState(&dst.x, &dst.y) & SDL_BUTTON(SDL_BUTTON_LEFT))
+			if ((SDL_GetMouseState(&dst.x, &dst.y) & SDL_BUTTON(SDL_BUTTON_LEFT)) || event.key.keysym.sym == SDLK_SPACE)
 				pixel[dst.y][dst.x]->filled = 1;
 		}
 		if (state[SDL_SCANCODE_ESCAPE])
 			quit = 1;
 		update_sand_on_screen(sdl, pixel);
-		SDL_Delay(1);
+	//	SDL_Delay(1);
 	}
 	clean_before_exiting(sdl, pixel);
 }
